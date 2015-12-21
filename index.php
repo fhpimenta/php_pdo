85cd172243ebe71d85e130a8abfce420885b61ae
Felipe Pimenta <fhpimenta12@gmail.com>
2015-12-21 17:32:07 -0300

    Fim da Parte 1 do curso

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-$conexao = new \PDO("mysql:host=localhost;dbname=pdo", "root", "1234");
+try {
+	$conexao = new \PDO("mysql:host=localhost;dbname=pdo", "root", "1234");
+
+	#$query = "INSERT INTO clientes(name, email) VALUES ('Felipe Pimenta', 'fhpimenta12@gmail.com')";
+	$query = "select * from clientes where id=:id";
+
+	$stmt = $conexao->prepare($query);
+	$stmt->bindValue(":id", $_GET['id'], PDO::PARAM_INT);
+	$stmt->execute();
+	
+	print_r($stmt->fetch(PDO::FETCH_ASSOC));
+
+}catch(\PDOException $e) {
+	echo "Não foi possivel estabelecer uma conexão com o banco de dados. Erro: código ". $e->getCode();
+}
